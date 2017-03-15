acd3f135cc0eb2dcafd21b596005d669e029f7fa
geralt <patrykbar97@gmial.com>
2017-03-15 21:38:03 +0100

    Zadanie 6

---
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.h
@@ -0,0 +1,10 @@
+#ifndef JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+#include <cstdint>
+#include <string>
+#include <math.h>
+#define JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive);
+
+
+#endif //JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
