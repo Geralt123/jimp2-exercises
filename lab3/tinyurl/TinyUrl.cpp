a5e2b9c7f33dc02ea92b53af8a373f4a3a65124a
geralt <patrykbar97@gmial.com>
2017-03-23 00:57:39 +0100

    Zadanie 1

---
+++ lab3/tinyurl/TinyUrl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by geralt on 21.03.17.
+//
+
+#include "TinyUrl.h"
---
+++ lab3/tinyurl/TinyUrl.h
@@ -0,0 +1,14 @@
+//
+// Created by geralt on 21.03.17.
+//
+
+#ifndef JIMP_EXERCISES_TINYURL_H
+#define JIMP_EXERCISES_TINYURL_H
+
+
+class TinyUrl {
+
+};
+
+
+#endif //JIMP_EXERCISES_TINYURL_H
